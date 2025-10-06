099d226c25116330cdc39d0281ab537edfce8386
rhv1501 <rhv4748@gmail.com>
2025-10-06 19:07:23 +0530

    fix of home page links

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect, Suspense, lazy } from "react";
 import { ScrollToTop } from "./components/ScrollToTop";
 import FloatingChatButton from "./components/FloatingChatButton";
